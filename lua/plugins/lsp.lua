6d4fb50097d0162f8774855d0c13cd459364b75a
bwintertkb <bartekwinter3@gmail.com>
2025-12-20 22:30:41 +0000

    Batman

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,15 +6,25 @@ return {
 		"saghen/blink.cmp",
 	},
 	config = function()
-		-- 1. General Keymaps (Run when ANY LSP attaches)
+		-- 1. Keymaps (Run when an LSP attaches to a buffer)
 		vim.api.nvim_create_autocmd('LspAttach', {
 			desc = 'LSP actions',
 			callback = function(event)
 				local opts = { buffer = event.buf, noremap = true, silent = true }
+				vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+				vim.keymap.set('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+				vim.keymap.set('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
 				vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+				vim.keymap.set('n', 'gc', '<cmd>lua vim.lsp.buf.incoming_calls()<cr>', opts)
+				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+				vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
+				vim.keymap.set('n', 'gw', '<cmd>lua vim.lsp.buf.workspace_symbol()<cr>', opts)
+				vim.keymap.set('n', '[x', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+				vim.keymap.set('n', ']x', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+				vim.keymap.set('n', ']s', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
 				vim.keymap.set('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-				-- Add your other keymaps here...
+				vim.keymap.set('n', '<leader>p', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
 			end,
 		})
 
